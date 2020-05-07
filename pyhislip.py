e813a0ad6ea3d7ee830395dd28303eb6b948c6f1
Noboru Yamamoto <noboru.yamamoto@kek.jp>
2020-05-07 14:10:06 +0900

    some modifications

--- pyhislip.py
+++ pyhislip.py
@@ -6,6 +6,8 @@ This module provide HiSLIP protocol client work.
 Created on 28 feb 2017
 
 @author: Levshinovskiy Mikhail
+
+modified by N.Yamamoto@kek.jp May 7,2020.
 '''
 
 import socket
@@ -13,7 +15,7 @@ import struct
 from time import sleep
 
 from collections import OrderedDict
-
+from select import poll, POLLIN, POLLPRI
 
 class HiSLIPFatalError(Exception):
     '''
@@ -24,7 +26,6 @@ class HiSLIPFatalError(Exception):
         self.expression = expression
         self.message = message
 
-
 class HiSLIPError(Exception):
     '''
     Exception raised up for HiSLIP errors
@@ -34,45 +35,26 @@ class HiSLIPError(Exception):
         self.expression = expression
         self.message = message
 
-
-class _HiSLIP():
+class _HiSLIP(object):
     '''
     This class is abstract and content main methods and attributes for HiSLIP protocol
     '''
+    # class constants.
+    # Maximum supported protocol version
+    _PROTOCOL_VERSION_MAX = 257  # <major><minor> = <1><1> that is 257
+    _INITIAL_MESSAGE_ID = 0xffffff00
+    _UNKNOWN_MESSAGE_ID  = 0xffffffff
+    
+    # Maximum Message Size
+    _MAXIMUM_MESSAGE_SIZE = 272  # Following VISA 256 bytes + header length 16 bytes
 
-    def __init__(self):
+    # Socket timeout
+    _SOCKET_TIMEOUT = 1
 
-        # Initializate constants
-        self._init_constants()
-
-        # Create a dictionary that storage HiSLIP message types
-        self.message_types = self._init_message_types()
-
-        # Create a dictionary that storage HiSLIP fatal error codes
-        self.fatal_error_codes = self._init_fatal_error_codes()
-
-        # Create a dictionary that storage HiSLIP error codes
-        self.error_codes = self._init_error_codes()
-
-    def _init_constants(self):
-        '''
-        Initialize preset values of some parameters
-        '''
-
-        # Maximum supported protocol version
-        self.PROTOCOL_VERSION_MAX = 257  # <major><minor> = <1><1> that is 257
-
-        # Maximum Message Size
-        self.MAXIMUM_MESSAGE_SIZE = 272  # Following VISA 256 bytes + header length 16 bytes
-
-        # Socket timeout
-        self.SOCKET_TIMEOUT = 1
-
-        # Lock timeout
-        self.LOCK_TIMEOUT = 3000
-
-    def _init_message_types(self):
-        '''
+    # Lock timeout
+    _LOCK_TIMEOUT = 3000
+    #
+    '''
         Initialize dictionary, that storage HiSLIP message types
 
         Comment use order "Sender", "Channel"
@@ -84,65 +66,66 @@ class _HiSLIP():
             S indicates Synchronous channel message
             A indicates Asynchronous channel message
             E indicates A message that may be send on either the synchronous or asynchronous channel
-        '''
-        message_types = dict()
-        message_types["Initialize"] = 0                          # C, S
-        message_types["InitializeResponse"] = 1                  # S, S
-        message_types["FatalError"] = 2                          # E, E
-        message_types["Error"] = 3                               # E, E
-        message_types["AsyncLock"] = 4                           # C, S
-        message_types["AsyncLockResponse"] = 5                   # S, A
-        message_types["Data"] = 6                                # E, S
-        message_types["DataEnd"] = 7                             # E, S
-        message_types["DeviceClearComplete"] = 8                 # C, S
-        message_types["DeviceClearAcknowledge"] = 9              # S, S
-        message_types["AsyncRemoteLocalControl"] = 10            # C, A
-        message_types["AsyncRemoteLocalResponse"] = 11           # S, A
-        message_types["Trigger"] = 12                            # C, S
-        message_types["Interrupted"] = 13                        # S, S
-        message_types["AsyncInterrupted"] = 14                   # S, A
-        message_types["AsyncMaximumMessageSize"] = 15            # C, A
-        message_types["AsyncMaximumMessageSizeResponse"] = 16    # S, A
-        message_types["AsyncInitialize"] = 17                    # C, A
-        message_types["AsyncInitializeResponse"] = 18            # S, A
-        message_types["AsyncDeviceClear"] = 19                   # C, A
-        message_types["AsyncServiceRequest"] = 20                # S, A
-        message_types["AsyncStatusQuery"] = 21                   # C, A
-        message_types["AsyncStatusResponse"] = 22                # S, A
-        message_types["AsyncDeviceClearAcknowledge"] = 23        # S, A
-        message_types["AsyncLockInfo"] = 24                      # C, A
-        message_types["AsyncLockInfoResponse"] = 25              # S, A
-        # reserved for future use: 26 - 127
-        # VendorSpecific: 128 - 255
-
-        return message_types
-
-    def _init_fatal_error_codes(self):
-        '''
+    '''
+    message_types=OrderedDict((
+        ('Initialize', 0),                          # C, S
+        ('InitializeResponse', 1),                  # S, S
+        ('FatalError', 2),                          # E, E
+        ('Error', 3),                               # E, E
+        ('AsyncLock', 4),                           # C, S
+        ('AsyncLockResponse', 5),                   # S, A
+        ('Data', 6),                                # E, S
+        ('DataEnd', 7),                             # E, S
+        ('DeviceClearComplete', 8),                 # C, S
+        ('DeviceClearAcknowledge', 9),              # S, S
+        ('AsyncRemoteLocalControl', 10),            # C, A
+        ('AsyncRemoteLocalResponse', 11),           # S, A
+        ('Trigger', 12),                            # C, S
+        ('Interrupted', 13),                        # S, S
+        ('AsyncInterrupted', 14),                   # S, A
+        ('AsyncMaximumMessageSize', 15),            # C, A
+        ('AsyncMaximumMessageSizeResponse', 16),    # S, A
+        ('AsyncInitialize', 17),                    # C, A
+        ('AsyncInitializeResponse', 18),            # S, A
+        ('AsyncDeviceClear', 19),                   # C, A
+        ('AsyncServiceRequest', 20),                # S, A
+        ('AsyncStatusQuery', 21),                   # C, A
+        ('AsyncStatusResponse', 22),                # S, A
+        ('AsyncDeviceClearAcknowledge', 23),        # S, A
+        ('AsyncLockInfo', 24),                      # C, A
+        ('AsyncLockInfoResponse', 25)               # S, A
+    ))
+    #
+    '''
         Create a list that storage HiSLIP fatal error codes
-        '''
-        fatal_error_codes = list()
-        fatal_error_codes.append('Unidentified error')  # 0
-        fatal_error_codes.append('Poorly formed message header')  # 1
-        fatal_error_codes.append('Attempt to use connection without both channels established')  # 2
-        fatal_error_codes.append('Invalid Initialization Sequence')  # 3
-        fatal_error_codes.append('Server refused connection due to maximum number of clients exceeded')  # 4
+    '''
+    fatal_error_codes = [
+        'Unidentified error',                                          #0
+        'Poorly formed message header',                                #1  
+        'Attempt to use connection without both channels established', #2
+        'Invalid Initialization Sequence',                             #3
+        'Server refused connection due to maximum number of clients exceeded' #4
         # 5..127 reserved for HiSLIP extensions
         # 128..255 Device-defined errors
-        return fatal_error_codes
+    ]
 
-    def _init_error_codes(self):
-        '''
+    '''
         Create a list that storage HiSLIP error codes
-        '''
-        error_codes = list()
-        error_codes.append('Unidentified error')  # 0
-        error_codes.append('Unrecognized Message Type')  # 1
-        error_codes.append('Unrecognized control code')  # 2
-        error_codes.append('Unrecognized Vendor Defined Message')  # 3
-        error_codes.append('Message too large')  # 4
+    '''
+    error_codes = [
+        'Unidentified error',               #0
+        'Unrecognized Message Type',        #1
+        'Unrecognized control code',        #2
+        'Unrecognized Vendor Defined Message',#3
+        'Message too large'                 #4
         # 5..127 reserved for HiSLIP extensions
         # 128..255 Device-defined errors
+    ]
+    
+    def __init__(self):
+        self.MAXIMUM_MESSAGE_SIZE=self._MAXIMUM_MESSAGE_SIZE
+        self.SOCKET_TIMEOUT = self._SOCKET_TIMEOUT
+        self.LOCK_TIMEOUT = self._LOCK_TIMEOUT 
 
     def _create_hislip_message(self, message_type, control_code=0, message_parameter=0, data=''):
         '''
@@ -257,8 +240,11 @@ class _HiSLIP():
         message = self._create_hislip_message(self.message_types['FatalError'], error_code)
         self.sync_channel.send(message)
         try:
+            peer=self.sync_channel.getpeername()
             self.sync_channel.close()
             self.async_channel.close()
+            # reconnect to server
+            self.connect(peer[0])
         except NameError:
             pass
 
@@ -283,8 +269,8 @@ class _HiSLIP():
     def _send_error_to_server(self, error_code):
         message = self._create_hislip_message(self.message_types['Error'], error_code)
         self.sync_channel.send(message)
-        self.sync_channel.close()
-        self.async_channel.close()
+        # self.sync_channel.close()
+        # self.async_channel.close()
         raise TypeError('Error with code ' + str(error_code))
 
     def _split_hislip_header(self, header, expected_message_type=-1):
@@ -312,10 +298,10 @@ class _HiSLIP():
             message['prologue'] = header[0:2]
 
             # Message Type
-            message['message_type'] = struct.unpack('>B', header[2:3])[0]
+            message['message_type'] = ord(header[2:3])
 
             # Control Code
-            message['control_code'] = struct.unpack('>B', header[3:4])[0]
+            message['control_code'] = ord(header[3:4])
 
             # Message parameter
             raw_message_parameter = header[4:8]
@@ -391,7 +377,6 @@ class _HiSLIP():
             data = str(struct.unpack('!q', raw_data)[0])
         else:
             data = raw_data.decode()
-
         return data
 
     def _read_hislip_message(self, sock, expected_message_type=-1):
@@ -403,13 +388,11 @@ class _HiSLIP():
 
         # read header and data
         try:
-
             raw_header = sock.recv(16)
             header = self._split_hislip_header(raw_header)
             if header['payload_length'] > 0:
-                raw_data = sock.recv(self.MAXIMUM_MESSAGE_SIZE)
+                raw_data = sock.recv(header['payload_length'])
                 data = self._read_hislip_data(raw_data, header['message_type'])
-                # data = raw_data.decode()
             else:
                 data = str()
         except socket.timeout:
@@ -432,13 +415,13 @@ class HiSLIP(_HiSLIP):
     '''
 
     def __init__(self):
-        super().__init__()
+        super(HiSLIP, self).__init__()
 
     def _read_hislip_message(self, sock, expected_message_type=-1):
         '''
             Except basic reading of HiSLIP message, we always shoud check RTM delivered from server
         '''
-        [header, data] = super()._read_hislip_message(sock, expected_message_type)
+        [header, data] = super(HiSLIP, self)._read_hislip_message(sock, expected_message_type)
         self.rmt_delivered = self._RMT_delivered(header['message_type'], data)
         return header, data
 
@@ -471,8 +454,10 @@ class HiSLIP(_HiSLIP):
         self.sync_channel.connect((ip, port))
 
         ''' Start the initialization, send protocol version and VendorID '''
-        message = self._create_hislip_message(self.message_types['Initialize'], 0, [257, vendor_id], sub_adress)
-
+        message = self._create_hislip_message(self.message_types['Initialize'],
+                                              0,
+                                              [self._PROTOCOL_VERSION_MAX, vendor_id],
+                                              sub_adress)
         self.sync_channel.send(message)
 
         ''' Get answer from server '''
@@ -495,11 +480,20 @@ class HiSLIP(_HiSLIP):
 
         ''' Get answer from server '''
         header = self._read_hislip_message(self.async_channel, self.message_types['AsyncInitializeResponse'])[0]
-
+        self.server_vendorID=header['message_parameter']
+        
         ''' Set work parameters '''
-        self.message_id = struct.unpack('>I', b'\xff\xff\xff\x00')[0]
+        self.message_id = self._INITIAL_MESSAGE_ID
         self.most_recent_message_id = self.message_id
         self.rmt_delivered = False
+
+        """ for poll waiting """
+        # poll object for sync_channel
+        self.sync_poll=poll()
+        self.sync_poll.register(self.sync_channel, POLLIN | POLLPRI)
+        # poll object for async_channel
+        self.async_poll=poll()
+        self.async_poll.register(self.async_channel, POLLIN | POLLPRI)
 
     def set_max_message_size(self, message_size):
         '''  This method set maximal available message size '''
@@ -522,26 +516,26 @@ class HiSLIP(_HiSLIP):
         header = self._read_hislip_message(self.async_channel, self.message_types['AsyncStatusResponse'])[0]
 
         status = header['control_code']
+        
+        # bin_status = str(bin(int(status)))[2:]
 
-        bin_status = str(bin(int(status)))[2:]
-
-        if len(bin_status) >= 5:
-            mav = int(bin_status[len(bin_status) - 5])
-        else:
-            mav = 0
-
+        # if len(bin_status) >= 5:
+        #     mav = int(bin_status[len(bin_status) - 5])
+        # else:
+        #     mav = 0
+        status &= 0xff # a contro_code is one byte data.
+        mav = status & 0x10 # select bit 4
+        #print "status:",mav,status
         return mav, status
 
     def _wait_for_answer(self):
-        ''' '''
-        mav = 0
-        from time import time
-        start_time = time()
-        timeout = time() - start_time
-        while not mav and timeout < 180:
-            mav = self.status_query()[0]
-            timeout = time() - start_time
-            sleep(0.1)
+        ''' wait reply from server'''
+        res=self.sync_poll.poll(self.LOCK_TIMEOUT)
+        return res
+
+    def increment_message_id(self):#3.1.2 Synchronized Mode Client Requirements
+        self.most_recent_message_id = self.message_id
+        self.message_id = (self.message_id + 2) & 0xffffffff # for wrap-around
 
     def write(self, data_str):
         ''' Method send "data" to server. "data" is string '''
@@ -567,8 +561,7 @@ class HiSLIP(_HiSLIP):
 
             self.sync_channel.send(message)
 
-            self.most_recent_message_id = self.message_id
-            self.message_id = self.message_id + 2
+            self.increment_message_id()
 
     def ask(self, data_str):
         ''' Method send query to server and read answer '''
@@ -584,7 +577,11 @@ class HiSLIP(_HiSLIP):
         eom = False
         while not eom:
             [header, data] = self._read_hislip_message(self.sync_channel)
-            if (header['message_parameter'] == self.most_recent_message_id) or (not self.overlap_mode and header['message_parameter'] == struct.unpack('>I', b'\xff\xff\xff\xff')[0]):
+            if ((header['message_parameter'] == self.most_recent_message_id)
+                or ( not self.overlap_mode and
+                     (header['message_parameter'] == self._UNKNOWN_MESSAGE_ID)
+                )
+            ):
                 if header['message_type'] == self.message_types['Data']:
                     full_data = full_data + data
                 elif header['message_type'] == self.message_types['DataEnd']:
@@ -595,7 +592,7 @@ class HiSLIP(_HiSLIP):
                     full_data = str()
                     break
             else:
-                self._raise_error(1, 1)
+                #self._raise_error(1, 1)
                 full_data = str()
                 break
         return full_data
@@ -634,15 +631,16 @@ class HiSLIP(_HiSLIP):
 
         self.overlap_mode = feature_setting
 
-        self.message_id = struct.unpack('>I', b'\xff\xff\xff\x00')[0]
+        self.message_id = self._INITIAL_MESSAGE_ID
+        self.most_recent_message_id = 0
+        self.rmt_delivered = False
 
     def trigger_message(self):
         ''' This method emulate a GPIB Group Execute Trigger '''
         message = self._create_hislip_message(self.message_types['Trigger'], self.rmt_delivered, self.message_id)
         self.sync_channel.send(message)
-
-        self.most_recent_message_id = self.message_id
-        self.message_id = self.message_id + 2
+        
+        self.increment_message_id()
 
     def remote_local(self, request):
         '''
@@ -681,8 +679,9 @@ class HiSLIP(_HiSLIP):
 
     def release_lock(self):
         ''' this method release lock from server '''
-        if self.most_recent_message_id == struct.unpack('>I', b'\xff\xff\xff\x00')[0]:
-            message_id = self.most_recent_message_id - 2
+        if self.most_recent_message_id == self._INITIAL_MESSAGE_ID:
+            #message_id = self.most_recent_message_id - 2 ?
+            message_id = 0
         else:
             message_id = self.most_recent_message_id
 
